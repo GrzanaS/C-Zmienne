f2a89eb2f342deb549d5cde927e05805041cd710
GrzegorzSzudzik <22676860-GrzegorzSzudzik@users.noreply.replit.com>
2023-04-29 06:20:01 +0000

    Mistake corrected

--- .ccls-cache/@home@runner@Tutorial01/main.c
+++ .ccls-cache/@home@runner@Tutorial01/main.c
@@ -5,6 +5,6 @@ int main(void)
   int x=5, y=7;
   const float pi=3.1419;
   
-  printf("Jak widzisz kurwa y to tak naprawdę widzisz %d\n", pi);
+  printf("Jak widzisz kurwa y to tak naprawdę widzisz %f\n", pi);
   return 0;
 }
